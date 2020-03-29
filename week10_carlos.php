383145557796a82c3d9abd6c0e08a267feea9afa
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-03-29 01:00:17 -0400

    final code

--- week10_carlos.php
+++ week10_carlos.php
@@ -1,3 +1,10 @@
+<?php
+  // buffering output in order to be able
+  // to change cookies within the html code
+  // then outputing all buffering
+  ob_start();
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,10 +14,11 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="main.css"/>
+    <title>INFO1208 - LAB09</title>
   </head>
   <body>
+
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">INFO1208 - PHP - LAB09</h1>
@@ -43,131 +51,293 @@
       </div><!-- end of card-body-->
     </div><!-- end of card-->
     <?php 
-    
-
-    //Work with the unix timestamp https://www.php.net/manual/en/function.time.php
-    echo "Unix time right now is: " . time() . "<br/>"; //refresh your browser to watch this value update
-    echo "The GMT time right now is: " . date("H:m:s", time()) . "<br/>";
-
-    // 3 marks
-    $twoHours = 2*60*60;
-    echo "The GTM time in 2 hours from now is: " . date("H:m:s", time() + $twoHours) . "<br/>";//check the php manual
-
-    echo "<hr>"; //some spacing
-
-    // Generate a unix timestamp https://www.php.net/manual/en/function.mktime.php
-    echo "Unix time for 6pm UTC March 25,2020 is: " . mktime(19,0,0,3,25,2020), "<br/>";
-
-    //fill in missing values and check against https://www.unixtimestamp.com/index.php to verify answer
-
-    // 6 marks
-    echo "Unix time for 3am UTC December 25,2021 is: " . mktime(), "<br/><br/>";  //
-
-    echo "<hr>"; //some spacing
-
-    //Notice you can use commas in PHP to output a list of items
-    //REview the following, change some of these values, reload and observe output
-    //verify outputs against https://www.unixtimestamp.com/index.php
-    echo strtotime("now"), "<br/>";
-    echo strtotime("10 September 2000"), "<br/>";
-    echo strtotime("+1 day"), "<br/>";
-    echo strtotime("+1 week"), "<br/>";
-    echo strtotime("+1 week 2 days 4 hours 2 seconds"), "<br/>";
-    echo strtotime("next Thursday"), "<br/>";
-    echo strtotime("last Monday"), "<br/>";
-
-    //Try any 2 strings and verify your results are working
-    //2 marks
-    echo "Ryan Price strtotime tests<br/>";
-    echo strtotime(""), "<br/>";
-    echo strtotime(""), "<br/><br/>";
-
-    echo "<hr>"; //some spacing
-
-    // isset() https://www.php.net/manual/en/function.isset.php and unset() https://www.php.net/manual/en/function.unset.php
-    $my_Array['my_key'] = "Got something";
-    $b = 0;
-
-    echo 'val of $my_Array["my_key"] is:';
-    var_dump($my_Array['my_key']);
-    echo "<br/>val of b is:";
-    var_dump($b);
-    echo "<br/><br/>";
-
-    //WRONG
-    if($b){
-      echo 'WRONG:b is set';
-    }else{
-      echo 'WRONG:b is NOT set:';
-      var_dump($b);
-    }
-
-    //CORRECT way to verify a variable is declared, ALWAYS use isset() to check if a var has been declared
-    if(isset($b)){
-      echo 'CORRECT:b is set:';
-      var_dump($b);
-    }else{
-      echo 'CORRECT:b is NOT set:';
-      var_dump($b);
-    }
-
-    //now we can destroy $b and see what we get
-    unset($b);
-    if(isset($b)){
-      echo 'CORRECT:b is set:';
-      var_dump($b);
-    }else{
-      echo 'CORRECT:b is NOT set:';
-      var_dump($b);
-    }
-
-    echo "<hr>"; //some spacing
-    //Change $a to the value zero and do an if/else statement to output the value of $a
-    // 5 marks
-    /*
-    if(){
-
-
-    }else{
-
-    }
-    */
-
-    echo "<hr>"; //some spacing
-    //How to use setcookie() https://www.php.net/manual/en/function.setcookie.php
-
-    echo 'contents of $_COOKIE' . "<br/>";
-    var_dump($_COOKIE);
-    //set a cookie with an expiration of now + 3600 seconds = 1 week
-    //setcookie("ryan_cookie","value of ryan cookie", time() + 3600); //uncomment this line, and refresh
-    echo 'contents of $_COOKIE after' . "<br/>";
-    var_dump($_COOKIE);
-    //now comment out the setcookie() 3 lines above and relaod... you are seeing the cookie from the browser
-
-    //to REMOVE a cookie, setcookie() with a time parameter set to the past
-    setcookie('ryan_cookie', "", time() - 3600); //UNCOMMENT and refresh
-
-
-    //set your own cookie to expire in 3 days (3 marks)
-
-    //delete your cookie (3 marks)
-
-    echo "SESSION<hr>"; //some spacing
-
-    //Very similar to cookies you can set session variables and delete them
-    //  docs are here session_start() https://www.php.net/manual/en/function.session-start
-    session_start(); //uncomment, session_start() IS MANDATORY to do any sessions stuff !!!!
-    echo 'SESSION dump contains:';
-    var_dump($_SESSION);
-
-    echo 'The session ID is:' . session_id(); // refresh.. this does not change, it is unique and almost impossible to guess
-
-    //store something in our session using $_SESSION['your_key'] = "your val"; (2 marks)
 
     ?>
+    <div class="card">
+      <div class="card-header">
+        3 marks
+      </div><!-- end of card-header -->
+      <div class="card-body">
+        <?php
+           //Work with the unix timestamp https://www.php.net/manual/en/function.time.php
+          echo "Unix time right now is: " . time() . "<br/>"; //refresh your browser to watch this value update
+          echo "The time right now is: " . date("H:m:s", time()) . " (UTC)<br/>";
 
-    Now go see check to see if we can retreive our sessions variable in the <a href="week10_sessiontest_solutions.php">next page</a>
+          // 3 marks
+          $twoHours = 2*60*60;
+          echo "The time in 2 hours from now is: " . date("H:m:s", time() + $twoHours) . " (UTC) <br/>";//check the php manual
+        ?>
+      </div><!-- end of card-body-->
+    </div><!-- end of card-->
+    <?php 
+
+    ?>
+    <div class="card">
+      <div class="card-header">
+        6 marks
+      </div><!-- end of card-header -->
+      <div class="card-body">
+        <?php
+          // Generate a unix timestamp https://www.php.net/manual/en/function.mktime.php
+          echo "Unix time for 6pm UTC March 25,2020 is: " . mktime(18,0,0,3,25,2020), "<br/>";
+
+          //fill in missing values and check against https://www.unixtimestamp.com/index.php to verify answer
+
+          // 6 marks
+          echo "Unix time for 3am UTC December 25,2021 is: " . mktime(3,0,0,12,25,2021), "<br/><br/>";  //
+
+        ?>
+      </div><!-- end of card-body-->
+    </div><!-- end of card-->
+    <?php 
+
+    ?>
+    <div class="card">
+      <div class="card-header">
+        2 marks
+      </div><!-- end of card-header -->
+      <div class="card-body">
+        <ul class="list-group">
+          <?php
+            //Notice you can use commas in PHP to output a list of items
+            //REview the following, change some of these values, reload and observe output
+            //verify outputs against https://www.unixtimestamp.com/index.php
+          ?>
+          <li class="list-group-item"><?php echo strtotime("now"); ?><span class="badge badge-primary">now</span></li>
+          <li class="list-group-item"><?php echo strtotime("10 September 2000"); ?><span class="badge badge-primary">10 September 2000</span></li>
+          <li class="list-group-item"><?php echo strtotime("+1 day"); ?><span class="badge badge-primary">+1 day</span></li>
+          <li class="list-group-item"><?php echo strtotime("+1 week"); ?><span class="badge badge-primary">+1 week</span></li>
+          <li class="list-group-item"><?php echo strtotime("+1 week 2 days 4 hours 2 seconds"); ?><span class="badge badge-primary">+1 week 2 days 4 hours 2 seconds</span></li>
+          <li class="list-group-item"><?php echo strtotime("next Thursday"); ?><span class="badge badge-primary">next Thrusday</span></li>
+          <li class="list-group-item"><?php echo strtotime("last Monday"); ?><span class="badge badge-primary">last Monday</span></li>
+          <?php 
+            //Try any 2 strings and verify your results are working
+            //2 marks
+          ?>
+          <li class="list-group-item h3"><?php echo "Carlos Ferraz strtotime tests"; ?><span class="badge badge-primary"></span></li>
+          <li class="list-group-item"><?php echo strtotime("+1 month"); ?><span class="badge badge-primary">+1 month</span></li>
+          <li class="list-group-item"><?php echo strtotime("+1 year"); ?><span class="badge badge-primary">+1 year</span></li>
+        </ul>
+      </div><!-- end of card-body-->
+    </div><!-- end of card-->
+    <?php
+
+    ?>
+    <div class="card">
+      <div class="card-header">
+        5 marks
+      </div><!-- end of card-header -->
+      <div class="card-body">
+        <?php
+          // isset() https://www.php.net/manual/en/function.isset.php and unset() https://www.php.net/manual/en/function.unset.php
+          $my_Array['my_key'] = "Got something";
+          $b = 0;
+
+          echo 'val of $my_Array["my_key"] is:  ';
+          var_dump($my_Array['my_key']);
+
+          echo "<br/><br/>val of b is:  ";
+          var_dump($b);
+
+          echo "<br/><br/>";
+
+          ?>
+          <div class="card">
+            <div class="card-header">
+              <strong>Wrong way to test if $b is set. In this case, $b is set, but the test is in the wrong way</strong>
+            </div>
+            <div class="card-body">
+              <?php
+                //WRONG way to verify a variable is declared
+                if( $b ){
+                  echo '<span class="badge badge-danger">WRONG:</span>  b is set<br/><br/>';
+                } else {
+                  echo '<span class="badge badge-danger">WRONG:</span>  b is NOT set: ';
+                  var_dump($b);
+                }
+              ?>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-header">
+              <strong>Correct way to test if $b is set. In this case, $b is set, and the test is correct, using isset</strong>
+            </div>
+            <div class="card-body">
+              <?php
+                //CORRECT way to verify a variable is declared, ALWAYS use isset() to check if a var has been declared and is not NULL
+                if( isset( $b ) ){
+                  echo '<span class="badge badge-success">CORRECT:</span> b is set: ';
+                  var_dump( $b );
+                  echo "<br/><br/>";
+                } else {
+                  echo '<span class="badge badge-success">CORRECT:</span> b is NOT set:';
+                  var_dump( $b );
+                }
+              ?>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-header">
+              <strong>After $b is destroyed with unset and we keeping test the correct way, with isset</strong>
+            </div>
+            <div class="card-body">
+              <?php
+                //now we can destroy $b and see what we get
+                unset($b);
+                if(isset($b)){
+                  echo '<span class="badge badge-success">CORRECT:</span> b is set:';
+                  var_dump($b);
+                }else{
+                  echo '<span class="badge badge-success">CORRECT:</span> b is NOT set, so we get the message:';
+                  ?>
+                    <div class="card">
+                      <div class="card-body">
+                        <?php var_dump($b);?>
+                      </div>
+                    </div>
+                  <?php
+                }
+              ?>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-header">
+              <strong>Change $a to the value zero and do an if/else statement to output the value of $a</strong>
+            </div>
+            <div class="card-body">
+              <?php
+                //Change $a to the value zero and do an if/else statement to output the value of $a
+                // 5 marks
+
+                $a = 0;
+
+                if( isset ( $a ) ){
+
+                  echo '<span class="badge badge-success">SUCCESS:</span> a is set and its value is: ';
+                  var_dump( $a );
+
+                }else{
+
+                  echo '<span class="badge badge-danger">FAIL:</span> a is not set.';
+                  var_dump( $a );
+      
+                }
+
+              ?>
+            </div>
+          </div>
   
+      </div><!-- end of card-body-->
+    </div><!-- end of card--> 
+    
+    <div class="card">
+      <div class="card-header">
+        COOKIES!
+      </div><!-- end of card header-->
+      <div class="card-body">
+        <?php
+          //How to use setcookie() https://www.php.net/manual/en/function.setcookie.php
+          echo 'contents of $_COOKIE' . "<br/>";
+          var_dump($_COOKIE);
+          ?>
+            <div class="card">
+              <div class="card-header">
+                set a cookie with an expiration of now + 7*24*60*60 seconds = 1 week
+              </div>
+              <div class="card-body">
+                <?php
+                  //set a cookie with an expiration of now + 3600 seconds = 1 week
+                  setcookie('ryan_cookie', 'value of ryan cookie', time() + 7*24*60*60);
+                  echo 'contents of $_COOKIE after ryan_cookie' . "<br/>";
+                  var_dump($_COOKIE);
+                  //now comment out the setcookie() 3 lines above and relaod... you are seeing the cookie from the browser
+                ?>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header">
+                to REMOVE a cookie, setcookie() with a time parameter set to the past
+              </div>
+              <div class="card-body">
+                <?php
+                  setcookie('ryan_cookie','', time() - 3600);
+                  echo 'contents of $_COOKIE after ryan_cookie removal' . "<br/>";
+                  var_dump($_COOKIE);
+                  //now comment out the setcookie() 3 lines above and relaod... you are seeing the cookie from the browser
+                ?>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header">
+                set your own cookie to expire in 3 days (3 marks)
+              </div>
+              <div class="card-body">
+                <?php
+                  setcookie('carlos_cookie', 'content of carlos_cookie', time()+3*24*60*60);
+                  echo 'contents of $_COOKIE after carlos_cookie' . "<br/>";
+                  var_dump($_COOKIE);
+                  //now comment out the setcookie() 3 lines above and relaod... you are seeing the cookie from the browser
+                ?>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header">
+                delete your cookie (3 marks) commented on the code
+              </div>
+              <div class="card-body">
+                <?php
+                  //setcookie('carlos_cookie', '', time()-3600);
+                  echo 'contents of $_COOKIE after carlos_cookie removal' . "<br/>";
+                  var_dump($_COOKIE);
+                ?>
+              </div>
+            </div>
+            
+          </div> <!-- end of card body-->              
+    </div><!-- end of card-->
+
+    <div class="card">
+      <div class="card-header">
+        <strong>SESSIONS!</strong>
+      </div><!-- end of card header-->
+      <div class="card-body">
+        
+        <?php
+          // Very similar to cookies you can set session variables and delete them
+          // docs are here session_start() https://www.php.net/manual/en/function.session-start
+          session_start(); //uncomment, session_start() IS MANDATORY to do any sessions stuff !!!!
+        ?>
+        
+        <div class="card">
+          <div class="card-header"><strong>SESSION DUMP contains:</strong></div>
+          <div class="card-body"><?php var_dump($_SESSION);?></div>
+        </div>
+
+        <div class="card">
+          <div class="card-header"><strong>The SESSION ID is:</strong></div>
+          <div class="card-body"><?php echo session_id(); ?></div>
+        </div>
+
+        <div class="card">
+          <div class="card-header"><strong>Making a custom session variable: (2 marks)</strong></div>
+          <div class="card-body">$_SESSION['carlos_session_key'] = "carlos_session_value";</div>
+        
+              <?php
+                $_SESSION['carlos_session_key'] = "carlos_session_value";
+              ?>
+        
+        </div>
+
+      </div><!-- end of card body-->
+
+      <div class="card-footer">
+        <p>Now go see check to see if we can retreive our sessions variable in the <a class="btn btn-primary" href="week10_carlos_sessiontest.php">next page</a></p>
+      </div>
+    </div><!-- end of card-->
   </div>
 
 
@@ -179,3 +349,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+  ob_end_flush();
+?>
--- week10_carlos_sessiontest.php
+++ week10_carlos_sessiontest.php
@@ -1,8 +1,48 @@
 <?php
-//Check our session output
+    //Check our session output
 
-//start the session ( 1 mark )
+    //start the session ( 1 mark )
+    session_start();
 
-//output a user friendly message for your value (2 marks)
+    //output a user friendly message for your value (2 marks)
+
 ?>
-The value in my session is
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css"/>
+    <title>INFO1208 - LAB09</title>
+  </head>
+  <body>
+
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">INFO1208 - PHP - LAB09 - SESSION TEST</h1>
+      <p class="lead">From: Carlos Ferraz</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        The value of the custom session variable is:
+      </div><!-- end of card-header -->
+      <div class="card-body">
+        <?php
+            echo '$_SESSION["carlos_session_key"] = ' . $_SESSION['carlos_session_key'];
+        ?>
+      </div><!-- end of card-body-->
+    </div><!-- end of card-->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
